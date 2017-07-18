24da734b7752b16a19fcf5e4200bc13d87a3110d
Ali Yisa <aliyisa@outlook.com>
2017-07-18 23:12:58 +0100

    fix

--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-sm-2">
+    <div class="col-xs-2">
         <div class="thumbnail">
             <img class="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png">
         </div><!-- /thumbnail -->
